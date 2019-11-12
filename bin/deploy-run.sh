1aa93e6e40cafba5fdb24aecbfea6b4595ce42f6
Costin Bleotu <costi@databus.pro>
2019-11-12 16:07:30 +0200

    added ckan-odata

--- bin/deploy-run.sh
+++ bin/deploy-run.sh
@@ -5,6 +5,8 @@
 sudo /usr/lib/ckan/default/bin/pip install -e git+https://github.com/code4romania/ckanext-dataportaltheme.git#egg=ckanext-dataportaltheme
 sudo /usr/lib/ckan/default/bin/pip install -e git+https://github.com/code4romania/ckanext-mediumfeed.git#egg=ckanext-mediumfeed
 sudo /usr/lib/ckan/default/bin/pip install -e git+https://github.com/code4romania/ckanext-githubfeed.git#egg=ckanext-githubfeed
+sudo /usr/lib/ckan/default/bin/pip install -e git+https://github.com/OpenGov-OpenData/ckanext-odata#egg=ckanext-odata
+
 
 sudo service apache2 reload
 sudo service nginx reload
